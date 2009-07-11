18a0ca63d9bc69e965c64b6f2b2d5c3ee76b9e08
Yehuda Katz <wycats@gmail.com>
2009-07-11 17:22:49 -0400

    Add support for syntax that is closer to Python's semantics

--- decorators.rb
+++ decorators.rb
@@ -4,60 +4,78 @@ module MethodDecorators
       attr_accessor :decorated_methods
     end
   end
-  
+
+  def method_missing(name, *args, &blk)
+    if Object.const_defined?(name)
+      const = Object.const_get(name)
+    elsif Decorator.decorators.key?(name)
+      const = Decorator.decorators[name]
+    else
+      return super
+    end
+
+    instance_eval <<-ruby_eval, __FILE__, __LINE__ + 1
+      def #{name}(*args, &blk)
+        decorate(#{const.name}, *args, &blk)
+      end
+    ruby_eval
+
+    send(name, *args, &blk)
+  end
+
   def method_added(name)
     return unless @decorators
-    
+
     decorators = @decorators.dup
     @decorators = nil
     @decorated_methods ||= Hash.new {|h,k| h[k] = []}
-    
+
     class << self; attr_accessor :decorated_methods; end
-    
-    decorators.each do |klass|
-      decorator = klass.new(self, name)
+
+    decorators.each do |klass, args|
+      decorator = klass.respond_to?(:new) ? klass.new(self, instance_method(name), *args) : klass
       @decorated_methods[name] << decorator
     end
-    
-    alias_method "undecorated_#{name}", name
-    
+
     class_eval <<-ruby_eval, __FILE__, __LINE__ + 1
       def #{name}(*args, &blk)
+        ret = nil
         self.class.decorated_methods[#{name.inspect}].each do |decorator|
-          decorator.call(self, *args, &blk)
+          ret = decorator.call(self, *args, &blk)
         end
+        ret
       end
     ruby_eval
   end
-  
-  def decorate(klass)
+
+  def decorate(klass, *args)
     @decorators ||= []
-    @decorators << klass
+    @decorators << [klass, args]
   end
 end
 
 class Decorator
-  def self.decorator_name(name)
-    @decorator_name = name
+  class << self
+    attr_accessor :decorators
+    def decorator_name(name)
+      Decorator.decorators ||= {}
+      Decorator.decorators[name] = self
+    end
   end
-  
+
   def self.inherited(klass)
     name = klass.name.gsub(/^./) {|m| m.downcase}
 
     return if name =~ /^[^A-Za-z_]/ || name =~ /[^0-9A-Za-z_]/
-    
+
     MethodDecorators.module_eval <<-ruby_eval, __FILE__, __LINE__ + 1
       def #{klass}(*args, &blk)
         decorate(#{klass}, *args, &blk)
       end
     ruby_eval
   end
-  
-  def initialize(klass, meth)
-    @meth = meth
-  end
-  
-  def undecorated(obj, name)
-    obj.send("undecorated_#{name}")
+
+  def initialize(klass, method)
+    @method = method
   end
 end
--- specs.rb
+++ specs.rb
@@ -4,15 +4,15 @@ module Spec
   module Matchers
     def have_stdout(regex)
       regex = /^#{Regexp.escape(regex)}$/ if regex.is_a?(String)
-      
+
       simple_matcher("have_stdout") do |given, matcher|
         $stdout = StringIO.new
         given.call
         $stdout.rewind
         captured = $stdout.read
-        
+
         matcher.failure_message = "Expected #{regex} but got #{captured}"
-        
+
         $stdout = STDOUT
         captured =~ regex
       end
@@ -31,26 +31,26 @@ end
 describe "a class with simple method decorators" do
   class MyDecorator
     def initialize(klass, meth)
-      puts "Initializing MyDecorator for #{klass}##{meth}"
+      puts "Initializing MyDecorator for #{klass}"
       @meth = meth
     end
-    
+
     def call(this)
       puts "Inside MyDecorator#call"
     end
   end
-  
+
   class MyDecorator2
     def initialize(klass, meth)
-      puts "Initializing MyDecorator2 for #{klass}##{meth}"
+      puts "Initializing MyDecorator2 for #{klass}"
       @meth = meth
     end
-    
+
     def call(this)
       puts "Inside MyDecorator#call"
     end
   end
-  
+
   it "makes a new instance of the decorator when a decorated method is added" do
     lambda do
       class Simple1
@@ -61,9 +61,9 @@ describe "a class with simple method decorators" do
           puts "Inside a_function"
         end
       end
-    end.should have_stdout "Initializing MyDecorator for Simple1#a_function\n"
+    end.should have_stdout "Initializing MyDecorator for Simple1\n"
   end
-  
+
   it "initializes multiple decorators when a decorated method is added" do
     lambda do
       class Simple2
@@ -75,10 +75,10 @@ describe "a class with simple method decorators" do
           puts "Inside a_function"
         end
       end
-    end.should have_stdout "Initializing MyDecorator for Simple2#a_function\n" \
-                           "Initializing MyDecorator2 for Simple2#a_function\n"
+    end.should have_stdout "Initializing MyDecorator for Simple2\n" \
+                           "Initializing MyDecorator2 for Simple2\n"
   end
-  
+
   it "calls the decorator instead of the method" do
     silence_stdout do
       class Simple3
@@ -90,12 +90,12 @@ describe "a class with simple method decorators" do
         end
       end
     end
-    
+
     lambda do
       Simple3.new.a_function
     end.should have_stdout "Inside MyDecorator#call"
   end
-  
+
   it "can wrap a function" do
     lambda do
       class MyDecorator3 < Decorator
@@ -105,24 +105,24 @@ describe "a class with simple method decorators" do
 
         def call(this)
           puts "Before MyDecorator#call"
-          undecorated(this, @meth)
+          @meth.bind(this).call
           puts "After MyDecorator#call"
         end
       end
-      
+
       class Simple4
         extend MethodDecorators
-        
+
         decorate MyDecorator3
         def a_function
           puts "Inside a_function"
         end
       end
-      
+
       Simple4.new.a_function
     end.should have_stdout "Before MyDecorator#call\nInside a_function\nAfter MyDecorator#call"
   end
-  
+
   it "passes on any arguments to the method to decorator#call" do
     class MyDecorator4 < Decorator
       def call(this, *args)
@@ -130,21 +130,21 @@ describe "a class with simple method decorators" do
         puts "BLOCK" if block_given?
       end
     end
-    
+
     lambda do
       class Simple5
         extend MethodDecorators
-        
+
         decorate MyDecorator4
         def a_function
           puts "Inside a_function"
         end
       end
-      
+
       Simple5.new.a_function("omg", "omg") {}
     end.should have_stdout %{["omg", "omg"]\nBLOCK}
   end
-  
+
   it "supports a simpler syntax" do
     class MyDecorator5 < Decorator
       def call(this, *args)
@@ -152,42 +152,65 @@ describe "a class with simple method decorators" do
         puts "BLOCK" if block_given?
       end
     end
-    
+
     lambda do
       class Simple5
         extend MethodDecorators
-        
+
         MyDecorator5()
         def a_function
           puts "Inside a_function"
         end
       end
-      
+
       Simple5.new.a_function("omg", "omg") {}
     end.should have_stdout %{["omg", "omg"]\nBLOCK}
   end
-  
+
   it "supports giving the decorator a name" do
     module Omg
       class MyDecorator6 < Decorator
         decorator_name :surround_it
-        
+
         def call(this, *args)
           puts args.inspect
           puts "BLOCK" if block_given?
         end
       end
     end
-    
+
     lambda do
       class Simple6
         extend MethodDecorators
-        
+
         surround_it
         def a_function
           puts "Inside a_function"
         end
       end
     end
+  end
+
+  it "supports decorators with arguments" do
+    class ExtraParams
+      def initialize(klass, method, *args)
+        @method, @args = method, args
+      end
+
+      def call(this, *args)
+        @method.bind(this).call(*(args + @args))
+      end
+    end
+
+    class WithExtraParams
+      extend MethodDecorators
+
+      ExtraParams(:c, :d)
+      def function(a, b, c, d)
+        [a, b, c, d]
+      end
+    end
+
+    WithExtraParams.new.function(:a, :b).should == [:a, :b, :c, :d]
   end
 end
